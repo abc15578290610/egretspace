f4b068862de6301250f753db6bc1bb80d5c5c03f
zhong <1591944056@qq.com>
2019-01-21 11:57:19 +0800

    截图工具

--- Gofly/src/core/view/gameView.ts
+++ Gofly/src/core/view/gameView.ts
@@ -2,6 +2,8 @@ module game {
 	export class gameView extends ElementUI{
 		private m_back:eui.Label;//返回
 		private m_resume:eui.Label;//继续
+		private m_bg:eui.Image;//背景
+		private m_1:eui.Image;
 		private static _instance:gameView;
 		public static getInstance():gameView  
 		{  
@@ -14,9 +16,18 @@ module game {
 			this.skinName = "gameViewSkin";
 		}
 		protected init(){
-			setTimeout(function(){
-				EventManager.dispatchEventWith(EventNotify.GAME_RESULT,false,{dd:11});
-			},1000)
+			// setTimeout(function(){
+			// 	EventManager.dispatchEventWith(EventNotify.GAME_RESULT,false,{dd:11});
+			// },1000)
+			// this.m_bg
+			var img = this.drawImage(this.m_bg,this.m_1.x,this.m_1.y,this.m_1.width,this.m_1.height);
+		}
+		private drawImage(target: egret.DisplayObject,x?: number, y?: number, width?: number, height?: number){
+			var rect = new egret.Rectangle(x,y,width,height)
+			var rt:egret.RenderTexture = new egret.RenderTexture();
+            rt.drawToTexture(target,rect);
+			rt.toDataURL("image / png",rect);
+			this.m_1.texture = rt;
 		}
 		protected addEvent(){
 			this.m_back.addEventListener(egret.TouchEvent.TOUCH_TAP,this.handleEvent,this);
