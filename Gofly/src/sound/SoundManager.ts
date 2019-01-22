a330d36d40146bb9de952a3a8ce0a79b46798a35
zhongqing <1591944056@qq.com>
2019-01-22 23:05:38 +0800

    新增声音管理

---
+++ Gofly/src/sound/SoundManager.ts
@@ -0,0 +1,32 @@
+/**
+ * Created by Tint_ on 2017/4/17.
+ */
+class SoundManager{
+    /**
+     * 播放音效
+     * @param name 名字
+     * @param times 次数
+     * @param vol 音量 0 - 1
+     */
+    public static playSound(name:string, times:number = 0, vol:number = 1){
+        // let sound:egret.Sound = RES.getRes(name);
+        // sound.play(0, times).volume = vol;
+    }
+
+    /**
+     * 播放一次音效
+     * @param name 名字
+     * @param vol 音量 0 - 1
+     */
+    public static playSoundOnce(name:string, vol:number = 1){
+        SoundManager.playSound(name,1, vol);
+    }
+    /**
+     * 循环播放音乐
+     * @param name 名字
+     * @param vol 音量 0 - 1
+     */
+    public static playMusicLoops(name:string, vol:number = 1){
+        SoundManager.playSound(name,0, vol);
+    }
+}
